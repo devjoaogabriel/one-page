2e028b217c4853d960bf124f97cc41b38202eb23
devjoaogabriel <joao.gabrielmt32@gmail.com>
2022-09-15 16:11:43 -0300

    atualização de estilo

--- assets/js/style-components.js
+++ assets/js/style-components.js
@@ -6,7 +6,7 @@ ScrollReveal({
     // reset: true
 })
 
-ScrollReveal().reveal('.header-content, .image-header', {delay: 100, origin: 'top'})
-ScrollReveal().reveal('.article_class', {interval: 500, origin: 'left', delay: 100})
-ScrollReveal().reveal('.user_class, .email_class, .phone_class, .textarea_class, .button', {interval: 100, origin: 'left', delay: 100})
+ScrollReveal().reveal('.header-content, .image-header', {delay: 100, interval: 250, origin: 'top'})
+ScrollReveal().reveal('.effect', {interval: 500, origin: 'left', delay: 100})
+ScrollReveal().reveal('.user_class, .email_class, .phone_class, .textarea_class, .button, .title_form', {interval: 100, origin: 'top', delay: 100})
 
